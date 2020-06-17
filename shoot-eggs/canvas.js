250e8b5887e516bf805adaf71eb1a0ce978d02af
Kien <trungkien2895@gmail.com>
2020-06-17 14:44:51 +0700

    learning method rect

--- shoot-eggs/canvas.js
+++ shoot-eggs/canvas.js
@@ -1,9 +1,12 @@
 var canvas = document.getElementById("game");
 var context = canvas.getContext("2d");
 
-context.moveTo(0, 0);
-context.lineTo(100, 0);
-context.lineTo(100, 50);
-context.lineTo(0, 50);
-context.lineTo(0, 0);
+context.rect(0, 0, 50, 50);
 context.stroke();
+context.fillStyle = "blue";
+
+context.strokeStyle = "red";
+context.rect(0, 60, 100, 50);
+context.stroke();
+context.fillStyle = "red";
+context.fill();
